59d4d095549774f9e9cc8be1d1d09116e5b7f341
Korobenko Igor <igorkorobenko2000@gmail.com>
2024-10-28 13:38:50 +0100

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-function openModal(modalId) {
-    document.getElementById(modalId).style.display = 'block';
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = 'none';
-}
-
-// Закрытие модального окна при нажатии вне его
-window.onclick = function(event) {
-    const modals = document.querySelectorAll('.modal');
-    modals.forEach((modal) => {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    });
-};
+const quotes = [
+    "Только те, кто рискует зайти слишком далеко, могут узнать, как далеко можно зайти. - Т.S. Элиот",
+    "Великие дела начинаются с маленьких шагов.",
+    "Неудачи – это просто ступени к успеху.",
+    "Не сдавайтесь. Обычно последнее ключевое усилие – это ключ к успеху.",
+    "Учитесь на каждом шаге. Учёба никогда не заканчивается.",
+    "Верьте в себя и всегда продолжайте.",
+    "Секрет успеха в постоянстве цели.",
+    "Ваши возможности безграничны.",
+    "Ставьте цели и работайте, пока они не станут реальностью.",
+    "Успех – это путешествие, а не пункт назначения."
+];
 
 function getRandomQuote() {
-    const quotes = [
-        "Только те, кто рискует зайти слишком далеко, могут узнать, как далеко можно зайти. - Т.S. Элиот",
-        "Великие дела начинаются с маленьких шагов.",
-        "Неудачи – это просто ступени к успеху.",
-        "Не сдавайтесь. Обычно последнее ключевое усилие – это ключ к успеху.",
-        "Учитесь на каждом шаге. Учёба никогда не заканчивается.",
-        "Верьте в себя и всегда продолжайте.",
-        "Секрет успеха в постоянстве цели.",
-        "Ваши возможности безграничны.",
-        "Ставьте цели и работайте, пока они не станут реальностью.",
-        "Успех – это путешествие, а не пункт назначения."
-    ];
     const randomIndex = Math.floor(Math.random() * quotes.length);
     document.getElementById('quote-text').innerText = quotes[randomIndex];
 }
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Закрытие модального окна при нажатии вне его содержимого
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+
